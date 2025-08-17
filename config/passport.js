0fa8c263f5dc9db122f98d85093f48915b9cee52
blockchain_josh <94154157+Josh0007-sunday@users.noreply.github.com>
2025-08-17 22:47:12 +0100

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.GITHUB_CALLBACK_URL || 'http://localhost:5000/auth/github/callback',
+    callbackURL: process.env.GITHUB_CALLBACK_URL || '',
     scope: ['user:email', 'repo']
   },
   async (accessToken, refreshToken, profile, done) => {
