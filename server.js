8fc78e0d35751a8baa9719efe5e7e10715dd9465
blockchain_josh <94154157+Josh0007-sunday@users.noreply.github.com>
2025-08-17 21:10:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -67,9 +67,9 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-// const PORT = process.env.PORT || 5000;
-// // app.listen(PORT, () => {
-// //   console.log(`Server running on port ${PORT}`);
-// // });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
