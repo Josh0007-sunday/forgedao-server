9f3247d3ff9e745db7684a46f923bf6693348b0a
blockchain_josh <94154157+Josh0007-sunday@users.noreply.github.com>
2025-08-17 22:48:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ app.use(session({
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true,
-    domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : undefined
   }
 }));
 
